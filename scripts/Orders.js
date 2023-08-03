176dfded29abd4e49ce6c1f61da93d3beffcce83
Sanjeev10 <ksanjeev0359@gmail.com>
2023-08-03 20:52:14 +0530

    trying to get product into the orders js

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,6 +1,24 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+let headerSection;
 
+headerSection =`<div class="orderHeader-grid">
+<div class="left-section">
+  <div class="order-Date">
+    <div class="text-orderPlaced">Order Placed</div>
+    <div> Date: july</div>
+  </div>
+
+  <div class="order-Total">
+    <div class="text-Total">Total:</div>
+    <div>$222</div>
+  </div>
+</div>
+<div class="right-section">
+  <div class="text-OrderId">Order Id :</div>
+  <div>lmgfnfngsddfffdfnon-9</div>
+</div>
+</div>`
 
 
 let Summary ;
@@ -14,26 +32,31 @@ cart.forEach( cartItem  =>{
      }
     })
 
-Summary +=`<div class="product-image"> 
-<img class="css-product-image" src="${matchingItem.image}" alt="">
-</div>
-<div class="product-details"> 
-<div class="product-name">${matchingItem.name}</div>
-<div class="product-arrivingDate">Arriving on: July 25</div>
-<div class="product-size">Shoe size (US): ${matchingItem.priceCents}</div>
-<div class="product-quantity"> Quantity: ${cartItem.quantity}</div>
-<button class="product-BuyAgain">
-   <img class="image-BuyAgain" src="images/buy-again.png">
-   <span>Buy it again</span> 
-  </button>
-</div>
-<div>
-<button class="css-TrackPackage"> Track Your Package</button>
+Summary +=`  
+
+
+<div class="item-grid ">
+  <div class="product-image"> 
+     <img class="css-product-image" src="${matchingItem.image}" alt="">
+  </div>
+  <div class="product-details"> 
+  <div class="product-name">${matchingItem.name}</div>
+  <div class="product-arrivingDate">Arriving on: July 25</div>
+  <div class="product-size">Shoe size (US): ${matchingItem.priceCents}</div>
+  <div class="product-quantity"> Quantity: ${cartItem.quantity}</div>
+        <button class="product-BuyAgain">
+        <img class="image-BuyAgain" src="images/buy-again.png">
+        <span>Buy it again</span> 
+        </button>
+  </div>
+  <div>
+      <button class="css-TrackPackage"> Track Your Package</button>
+  </div>
 </div>`
 
 })
+console.log(headerSection+Summary)
 
-
-document.querySelector('.js-item-grid')
-.innerHTML=Summary
+  document.querySelector('.js-Order-summary')
+        .innerHTML=headerSection+Summary
 
